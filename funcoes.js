58a02d9461dd942db2351307f094c00ea508477e
jorgebarbosat <62555995+jorgebarbosat@users.noreply.github.com>
2020-06-08 10:59:28 -0300

    .

---
+++ funcoes.js
@@ -0,0 +1,4 @@
+var num, programa, tentativa, pontos;
+var gabarito = new Array(30);
+
+var gabarito021 = new Array('B','A','A','D','C','B','D','A','A','D','C','B','B','C','D','D','D','D','B','D','A','B','A','A','D','A','C','A','C','A','C','C');
