1461f97dc9a2e70468af4704b9eefca5b931d09f
jorgebarbosat <62555995+jorgebarbosat@users.noreply.github.com>
2020-06-08 13:33:42 -0300

    .

--- funcoes.js
+++ funcoes.js
@@ -1,4 +1,12 @@
 var num, programa, tentativa, pontos;
 var gabarito = new Array(30);
 
-var gabarito021 = new Array('B','A','A','D','C','B','D','A','A','D','C','B','B','C','D','D','D','D','B','D','A','B','A','A','D','A','C','A','C','A','C','C');
+var gabarito021 = new Array('B', 'A', 'A', 'D', 'C', 'B', 'D', 'A', 'A', 'D', 'C', 'B', 'B', 'C', 'D', 'D', 'D', 'D', 'B', 'D', 'A', 'B', 'A', 'A','D','A','C','A','C','A','C','C');
+var gabarito022 = new Array('B', 'D', 'A', 'A', 'A', 'B', 'D', 'A', 'C', 'A', 'C', 'C', 'B', 'D', 'D', 'B', 'C', 'D', 'C', 'D', 'D', 'D', 'A', 'C', 'A', 'A', 'B', 'C', 'A', 'A');
+var gabarito023 = new Array('B', 'A', 'D', 'B', 'B', 'B', 'C', 'D', 'C', 'D', 'D', 'D', 'A', 'C', 'A', 'B', 'A', 'D', 'B', 'A', 'D', 'B', 'B', 'D', 'D', 'C', 'B', 'D', 'B', 'C');
+var gabarito024 = new Array('B', 'A', 'D', 'B', 'B', 'D', 'C', 'C', 'C', 'B', 'A', 'D', 'B', 'A', 'D', 'B', 'D', 'A', 'B', 'B', 'D', 'A', 'B', 'D', 'A', 'B', 'C', 'C', 'D', 'D');
+var gabarito025 = new Array('C', 'C', 'D', 'D', 'A', 'A', 'C', 'D', 'B', 'B', 'D', 'D', 'B', 'B', 'D', 'D', 'C', 'B', 'B', 'B', 'D', 'A', 'B', 'D', 'D', 'D', 'C', 'D', 'C', 'B');
+
+function iniciar() {
+    
+}
