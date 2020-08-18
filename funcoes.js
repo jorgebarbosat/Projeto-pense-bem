9e02fc3b72dd1619373ab1728559a6d628d23d6b
jorgebarbosat <62555995+jorgebarbosat@users.noreply.github.com>
2020-08-18 16:23:03 -0300

    .

--- funcoes.js
+++ funcoes.js
@@ -8,5 +8,41 @@ var gabarito024 = new Array('B', 'A', 'D', 'B', 'B', 'D', 'C', 'C', 'C', 'B', 'A
 var gabarito025 = new Array('C', 'C', 'D', 'D', 'A', 'A', 'C', 'D', 'B', 'B', 'D', 'D', 'B', 'B', 'D', 'D', 'C', 'B', 'B', 'B', 'D', 'A', 'B', 'D', 'D', 'D', 'C', 'D', 'C', 'B');
 
 function iniciar() {
-    
+    programa = window.prompt("Digite o número do programa:", "");
+
+	if (programa == "021")
+		gabarito = gabarito021;
+	else if (programa == "022")
+		gabarito = gabarito022;
+	else if (programa == "023")
+		gabarito = gabarito023;
+	else if (programa == "024")
+		gabarito = gabarito024;
+	else if (programa == "025")
+		gabarito = gabarito025;
+	else {
+		window.alert("Número de Programa Inválido!!!\nDigite entre 021 e 025!!!");
+		iniciar();
+	}
+
+	pontos = 0;
+	num = 1;
+	tentativa = 1;
+
+	document.getElementById('visor1').value = programa + "->" + num + ": ";
+	document.getElementById('visor2').value = "Tentativa " + tentativa + " de 3";
+}
+
+Function; jogar(resposta); {
+    document.getElementById('visor1').value = programa + "->" + num + ":" + resposta;
+
+	if (resposta == gabarito[num-1])
+		pontos++;
+
+	num++;
+
+	if (num > 30) {
+		document.getElementById('visor1').value = "***FIM***";
+		document.getElementById('visor2').value = "Pontuação: " + pontos;
+	}
 }
